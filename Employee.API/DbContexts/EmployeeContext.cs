81f846931cfd058b31187ee2547603512a262a1a
Donald Musabelliu <donald.musabelliu@vandenbroele.be>
2024-01-21 22:58:04 +0100

    rename

---
+++ Employee.API/DbContexts/EmployeeContext.cs
@@ -0,0 +1,21 @@
+﻿using Employee.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee.API.DbContexts
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Entities.Employee> Employees { get; set; } = null!;
+
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) 
+            : base(options) 
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ Employee.API/Models/EmployeeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Employee.API.Models
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public long? Salary { get; set; }
+        public string Position { get; set; } = string.Empty;
+    }
+}
